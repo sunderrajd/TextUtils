85f8e6ba0cb7f1c1af9d01294715441d6e31e55d
D Sunder Raj <dsunderraj37@gmail.com>
2024-08-21 17:18:21 +0530

    Implemented ReactRouterDom to navigate

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-// import About from "./components/About";
+import About from "./components/About";
 import TextForm from "./components/TextForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <Navbar title="Textuils" AboutText="AboutTextutils" />
-
-      <div className="container  my-3">
-        <TextForm Heading="Enter the text to anaylize" />
-        {/* <About /> */}
+    <BrowserRouter>
+      <Navbar title="TextUtils" AboutText="About TextUtils" />
+      <div className="container my-3">
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route
+            exact
+            path="/"
+            element={<TextForm Heading="Enter the text to analyze" />}
+          />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 }
 
